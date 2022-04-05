666dfe2b6f0f4bc73b5bae0ddaab767592918d5f
Jinhyo Yoo <jh.yoo@pioncorp.com>
2022-04-05 17:19:20 +0900

    update

--- coffee-machine/src/page/Machine.tsx
+++ coffee-machine/src/page/Machine.tsx
@@ -2,37 +2,11 @@ import { useEffect, useState } from 'react'
 import { CoffeeMachine, machineInventory } from '../service/CoffeeMachine'
 
 
+
 function Machine() {
 
-    const store = new CoffeeMachine(machineInventory)
-
+    const [store,] = useState<CoffeeMachine>(new CoffeeMachine(machineInventory))
     const [message, setMessage] = useState<string>("what would you like to drink?")
-    const [salesCoffee, setSalesCoffee] = useState<number>(0)
-    const [salesCocoa, setSalesCocoa] = useState<number>(0)
-    const [salesLemonTea, setSalesLemonTea] = useState<number>(0)
-    const [stop, setStop] = useState<boolean>(false)
-
-    useEffect(() => {
-
-    }, [])
-
-    const getCoffee = () => {
-        store.onMakeDrink("coffee")
-        setSalesCoffee(salesCoffee + 1)
-        setMessage("enjoy your coffee~")
-    }
-
-    const getCocoa = () => {
-        store.onMakeDrink("cocoa")
-        setSalesCocoa(salesCocoa + 1)
-        setMessage("enjoy your cocoa~")
-    }
-
-    const getLemonTea = () => {
-        store.onMakeDrink("lemon-tea")
-        setSalesLemonTea(salesLemonTea + 1)
-        setMessage("enjoy your lemon tea~")
-    }
 
     return (
         <div className="App">
@@ -42,53 +16,24 @@ function Machine() {
                     {message}
                 </h4>
                 <section>
-                    <div>
-                        <button
-                            onClick={getCoffee}
-                            disabled={stop}
-                        >
-                            {store.cocoa < 15 ? "품절" : "커피"}
-                        </button>
-                        <p>
-                            {salesCoffee}
-                        </p>
-                        <small>
-                            원두 : {store.coffee}g
-                        </small>
-                    </div>
-                    <div>
-                        <button
-                            onClick={getCocoa}
-                            disabled={stop}
-                        >
-                            {store.cocoa < 20 ? "품절" : "코코아"}
-                        </button>
-                        <p>
-                            {salesCocoa}
-                        </p>
-                        <small>
-                            율무 : {store.cocoa}g
-                        </small>
-                    </div>
-                    <div>
-                        <button
-                            onClick={getLemonTea}
-                            disabled={stop}
-                        >
-                            {store.lemon < 30 ? "품절" : "레몬티"}
-                        </button>
-                        <p>
-                            {salesLemonTea}
-                        </p>
-                        <small>
-                            레몬 : {store.lemon}g
-                        </small>
-                    </div>
+                    {store.drinkMenus.map(menu => (
+                        <div key={menu.name}>
+                            <button
+                                onClick={() => menu.makeDrink}
+                                disabled={menu.isAvailable()}
+                            >
+                                {menu.isAvailable() ? "품절" : menu.name}
+                            </button>
+                            <p>
+                                {/* {salesCoffee} */}
+                            </p>
+                        </div>
+                    ))}
                 </section>
                 <footer>
-                    <div>컵 : {store.cup}ea</div>
-                    <div>물 : {store.water}ml</div>
-                    <div>매출 : {store.sales}원</div>
+                    <div>컵 : {store.inventory.cup}ea</div>
+                    <div>물 : {store.inventory.water}ml</div>
+                    <div>매출 : {store.inventory.sales}원</div>
                 </footer>
             </header>
         </div>
--- coffee-machine/src/service/CoffeeMachine.ts
+++ coffee-machine/src/service/CoffeeMachine.ts
@@ -13,14 +13,17 @@ interface IMakeDrink {
     coffee?: number
     cocoa?: number
     lemon?: number
+    milk?: number
+    jin?: number
 }
 
 interface IDrinkMenus {
     name: string
     price: number
     material: IMakeDrink
+    makeDrink: (machineInventory: IMachineInventory) => boolean
+    isAvailable: (machineInventory: IMachineInventory) => boolean
 }
-
 
 export const machineInventory: IMachineInventory = {
     cup: 10,
@@ -31,98 +34,155 @@ export const machineInventory: IMachineInventory = {
     sales: 0
 }
 
+class DrinkMenu implements IDrinkMenus {
+
+    constructor(
+        public name: string,
+        public price: number,
+        public material: IMakeDrink,
+        public inventory: IMachineInventory
+    ) { }
+
+    makeDrink = () => {
+        console.log("make Drink", this.name)
+        if (this.isAvailable()) {
+            return true
+        }
+        throw new Error('not enough material')
+    }
+
+    isAvailable = () => {
+        for (let materialKey of Object.keys(this.material)) {
+            if ((this.inventory as any)[materialKey] < (this.material as any)[materialKey]) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+class CustomDrinkMenu extends DrinkMenu {
+    makeDrink = () => {
+        //addition operation
+
+        return true
+    };
+}
+
+
+
 export class CoffeeMachine {
-    static drinkMenus: Array<IDrinkMenus> = [
-        {
-            name: 'coffee',
-            price: 300,
-            material: {
-                cup: 1,
-                water: 100,
-                coffee: 15
-            }
-        }, {
-            name: 'cocoa',
-            price: 400,
-            material: {
-                cup: 1,
-                water: 150,
-                cocoa: 20
-            }
-        }, {
-            name: 'lemon-tea',
-            price: 500,
-            material: {
-                cup: 1,
-                water: 200,
-                lemon: 30
-            }
-        }
-    ]
-
-    cup: number
-    water: number
-    coffee: number
-    cocoa: number
-    lemon: number
-    sales: number
-
+    drinkMenus: Array<IDrinkMenus> = [];
+    inventory: IMachineInventory
     constructor(resource: IMachineInventory) {
-        this.cup = resource.cup
-        this.water = resource.water
-        this.coffee = resource.coffee
-        this.cocoa = resource.cocoa
-        this.lemon = resource.lemon
-        this.sales = resource.sales
+        this.inventory = resource
+        this.drinkMenus = [
+            new DrinkMenu(
+                'coffee',
+                300,
+                {
+                    cup: 1,
+                    water: 100,
+                    coffee: 15,
+                },
+                resource
+            ),
+            new DrinkMenu(
+                'cocoa',
+                400,
+                {
+                    cup: 1,
+                    water: 150,
+                    coffee: 20,
+                },
+                resource
+            ),
+            new DrinkMenu(
+                'lemon-tea',
+                500,
+                {
+                    cup: 1,
+                    water: 200,
+                    coffee: 30,
+                },
+                resource
+            ),
+            // {
+            //     name: 'cocoa',
+            //     price: 400,
+            //     material: {
+            //         cup: 1,
+            //         water: 150,
+            //         cocoa: 20
+            //     },
+            //     makeDrink: () => {
+            //         this.inventory
+            //         return true
+            //     },
+            //     isAvailable: () => {
+            //         return true
+            //     }
+            // }, {
+            //     name: 'lemon-tea',
+            //     price: 500,
+            //     material: {
+            //         cup: 1,
+            //         water: 200,
+            //         lemon: 30
+            //     },
+            //     makeDrink: () => {
+            //         this.inventory
+            //         return true
+            //     },
+            //     isAvailable: () => {
+            //         return true
+            //     }
+            // }
+        ]
     }
 
-    // get selector(): number {
-    //     return this.cup, this.water, this.coffee, this.cocoa, this.lemon, this.sales
-    // }
+    // onMakeDrink(selected: string) {
+    //     const selectedMenu = CoffeeMachine.drinkMenus.find(item => item.name === selected)
+    //     if (selectedMenu) {
+    //         const { material } = selectedMenu
+    //         for (let materialKey of Object.keys(material)) {
+    //             if ((this.inventory as any)[materialKey] > 0) {
+    //                 (this.inventory as any)[materialKey] = (this.inventory as any)[materialKey] - (material as any)[materialKey]
+    //                 console.log(materialKey, (this.inventory as any)[materialKey])
+    //             }
+    //         }
+    //         if (this.inventory.cup > 0) {
+    //             this.inventory.cup = this.inventory.cup - material.cup
+    //             console.log("cup", this.inventory.cup)
+    //         }
 
-    // set selector(value: number) {
-    //     this._selector = value
-    // }
-
-    onMakeDrink(selected: string) {
-        const selectedMenu = CoffeeMachine.drinkMenus.find(item => item.name === selected)
-        if (selectedMenu) {
-            const { material } = selectedMenu
-            if (this.cup > 0) {
-                this.cup = this.cup - material.cup
-                console.log("cup", this.cup)
-            }
-
-            if (this.water >= material.water) {
-                this.water = this.water - material.water
-                console.log("water", this.water)
-            }
-
-            if (selected === "coffee" && material.coffee) {
-                if (this.coffee >= material.coffee) {
-                    this.coffee = this.coffee - material.coffee
-                    console.log("coffee", this.coffee)
-                }
-            }
-
-            if (selected === "cocoa" && material.cocoa) {
-                if (this.cocoa >= material.cocoa) {
-                    this.cocoa = this.cocoa - material.cocoa
-                    console.log("cocoa", this.cocoa)
-                }
-            }
-
-            if (selected === "lemon-tea" && material.lemon) {
-                if (this.lemon >= material.lemon) {
-                    this.lemon = this.lemon - material.lemon
-                    console.log("lemon", this.lemon)
-                }
-            }
-
-            this.sales = this.sales + selectedMenu.price
-        }
-    }
+    //         if (this.inventory.water >= material.water) {
+    //             this.inventory.water = this.inventory.water - material.water
+    //             console.log("water", this.inventory.water)
+    //         }
 
 
+    //         if (selected === "coffee" && material.coffee) {
+    //             if (this.inventory.coffee >= material.coffee) {
+    //                 this.inventory.coffee = this.inventory.coffee - material.coffee
+    //                 console.log("coffee", this.inventory.coffee)
+    //             }
+    //         }
 
+    //         if (selected === "cocoa" && material.cocoa) {
+    //             if (this.inventory.cocoa >= material.cocoa) {
+    //                 this.inventory.cocoa = this.inventory.cocoa - material.cocoa
+    //                 console.log("cocoa", this.inventory.cocoa)
+    //             }
+    //         }
+
+    //         if (selected === "lemon-tea" && material.lemon) {
+    //             if (this.inventory.lemon >= material.lemon) {
+    //                 this.inventory.lemon = this.inventory.lemon - material.lemon
+    //                 console.log("lemon", this.inventory.lemon)
+    //             }
+    //         }
+
+    //         this.inventory.sales = this.inventory.sales + selectedMenu.price
+    //     }
 }
